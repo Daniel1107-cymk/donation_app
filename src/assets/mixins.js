5f650895a806cd061b8f5cc3921aabcae00b481f
Daniel Lim <daniel@grip-principle.com>
2021-09-04 14:51:52 +0700

    add mixin or default css

---
+++ src/assets/mixins.js
@@ -0,0 +1,25 @@
+export const Mixins = {
+  bgPrimary: '#001E6C',
+  bgButtonPrimary: '#FFAA4C',
+  bgButtonSecondary: '#ABABAB',
+  textPrimary: '#000',
+  textSecondary: '#ABABAB',
+  textOrange: '#FFAA4C',
+  textWhite: '#FFFF',
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  label: {
+    color: '#FFF',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  inputTextContainer: {
+    backgroundColor: '#FFF',
+    borderBottomWidth: 0,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    height: 40,
+  },
+};
