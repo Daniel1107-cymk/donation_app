dcdfbff331ac7f367d3632354251201bc8f4f723
Daniel Lim <daniel@grip-principle.com>
2021-09-14 22:14:16 +0700

    add helpers

---
+++ src/helpers/tokenHandler.js
@@ -0,0 +1,23 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default class Token {
+  static async save(token, expire) {
+    const tokenExpired = Date.now() + expire * 1000;
+    await AsyncStorage.setItem('token', token);
+    await AsyncStorage.setItem('token_expired', tokenExpired.toString());
+  }
+
+  static async isExpired() {
+    const currentTime = Date.now();
+    const tokenExpired = await AsyncStorage.getItem('token_expired');
+    if (tokenExpired === null) {
+      return true;
+    }
+    return +currentTime >= +tokenExpired;
+  }
+
+  static async removeToken() {
+    await AsyncStorage.removeItem('token');
+    await AsyncStorage.removeItem('token_expired');
+  }
+}
