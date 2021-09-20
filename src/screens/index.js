c7e3d53adac8668abf17e1434c0f951d53ae6dea
Daniel Lim <daniel@grip-principle.com>
2021-09-20 20:36:33 +0700

    show icon in bottom bar

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {StyleSheet, View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HeaderBackground} from '@react-navigation/elements';
-import {Icon} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 // screen
 import Home from './home';
 import Donate from './donate';
@@ -23,20 +24,81 @@ const Index = () => {
           );
         },
         tabBarShowLabel: false,
+        tabBarStyle: {height: 60},
       }}>
       <Tab.Screen
         name="HomeMenu"
         component={Home}
         options={{
+          headerTitle: 'Home',
           tabBarIcon: ({focused, color, style}) => (
-            <Icon name="home" type="feather" color="#517fa4" />
+            <View
+              style={
+                focused
+                  ? [styles.iconContainer, {backgroundColor: '#035397'}]
+                  : [styles.iconContainer]
+              }>
+              <Icon
+                name="home"
+                size={20}
+                color={focused ? '#FFF' : '#000000'}
+              />
+            </View>
           ),
         }}
       />
-      <Tab.Screen name="DonateMenu" component={Donate} />
-      <Tab.Screen name="ProfileMenu" component={Profile} />
+      <Tab.Screen
+        name="DonateMenu"
+        component={Donate}
+        options={{
+          tabBarIcon: ({focused, color, style}) => (
+            <View
+              style={
+                focused
+                  ? [styles.iconContainer, {backgroundColor: '#035397'}]
+                  : [styles.iconContainer]
+              }>
+              <Icon
+                name="hand-holding-heart"
+                size={20}
+                color={focused ? '#FFF' : '#000000'}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ProfileMenu"
+        component={Profile}
+        options={{
+          tabBarIcon: ({focused, color, style}) => (
+            <View
+              style={
+                focused
+                  ? [styles.iconContainer, {backgroundColor: '#035397'}]
+                  : [styles.iconContainer]
+              }>
+              <Icon
+                name="user"
+                size={20}
+                color={focused ? '#FFF' : '#000000'}
+              />
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+});
 
 export default Index;
